65785012aea72008cd075a82b00900a14f6d5a9b
Adam Coulter <ads@me.com>
2021-07-06 18:19:06 +1000

    retry404 instead

--- client/client.go
+++ client/client.go
@@ -181,7 +181,7 @@ func (client *Client) send(label string, key string, retry bool, additionalDecor
 	var resp *http.Response
 	var retryDecorator autorest.SendDecorator
 	if retry {
-		retryDecorator = autorest.DoRetryForAttempts(5, 1*time.Second)
+		retryDecorator = autorest.DoRetryForStatusCodes(5, 1*time.Second, 404)
 		resp, err = client.Send(req, retryDecorator)
 	} else {
 		resp, err = client.Send(req)
